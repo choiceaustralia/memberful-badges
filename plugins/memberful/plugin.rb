d3b343dcf15223e070992b38e6861d9615ba7ab5
Rimian Perkins <hello@rimian.com.au>
2016-10-18 13:24:32 +1100

    bump minor

---
+++ plugins/memberful/plugin.rb
@@ -0,0 +1,13 @@
+# name: Memberful Integration
+# about: This adds an interface between Memberful and Discourse. It's a bit unconventional.
+# version: 0.4.3
+# authors: rimian
+
+after_initialize do
+  Discourse::Application.routes.append do
+    namespace :memberful, defaults: { format: 'json' } do
+      get 'status', action: :status, controller: :memberful
+      post 'memberful', action: :test, controller: :memberful
+    end
+  end
+end
