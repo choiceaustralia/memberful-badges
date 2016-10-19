4065a66e6ae139da3be5f694a9f0c04dc3b943b7
Rimian Perkins <hello@rimian.com.au>
2016-10-19 19:43:30 +1100

    Configure for Rspec and Factory Girl

--- memberful/lib/memberful/engine.rb
+++ memberful/lib/memberful/engine.rb
@@ -1,9 +1,15 @@
+# require dependencies declared in your gemspec
+Gem.loaded_specs['memberful'].dependencies.each do |d|
+  require d.name
+end
+
 module Memberful
   class Engine < ::Rails::Engine
     isolate_namespace Memberful
 
     config.generators do |g|
       g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
   end
 end
