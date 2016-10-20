9c4cd443136afa9a4af799e740c9b3bb9e28ee9e
Rimian Perkins <hello@rimian.com.au>
2016-10-20 12:11:39 +1100

    add status output

--- memberful/app/controllers/memberful/web_hooks_controller.rb
+++ memberful/app/controllers/memberful/web_hooks_controller.rb
@@ -2,5 +2,8 @@ require_dependency "memberful/application_controller"
 
 module Memberful
   class WebHooksController < ApplicationController
+    def status
+      render json: { version: Memberful::VERSION }
+    end
   end
 end
