3a58a966eeef5c12ebff6c0d79777ae2aecad02d
Rimian Perkins <hello@rimian.com.au>
2016-10-24 13:54:53 +1100

    only use the action controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ require 'shoulda-matchers'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -1,12 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
+# require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "memberful"
@@ -26,7 +25,6 @@ module TestApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
