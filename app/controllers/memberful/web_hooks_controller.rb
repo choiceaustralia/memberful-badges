85bd06bca8ddc61e80eaf8d75d90ee1370011952
Rimian Perkins <hello@rimian.com.au>
2016-11-08 08:59:55 +1100

    refactor and bump with nil user condition

--- app/controllers/memberful/web_hooks_controller.rb
+++ app/controllers/memberful/web_hooks_controller.rb
@@ -10,35 +10,37 @@ module Memberful
 
     def create
       data = JSON.parse(request.body.read)
+      user = find_user(data)
+
+      head :ok and return if user.nil?
 
       if event_order?(data)
-        user = User.find_by_email(data['order']['member']['email'])
+        badge = Badge.find_by_name('Consumer Defender')
 
-        unless user.nil?
+        BadgeGranter.grant(badge, user) if data['event'] == 'order.purchased'
 
-          badge = Badge.find_by_name('Consumer Defender')
-
-          BadgeGranter.grant(badge, user) if data['event'] == 'order.purchased'
-
-          if event_revoke?(data)
-            user_badge = UserBadge.find_by(badge_id: badge.id, user_id: user.id)
-            BadgeGranter.revoke(user_badge)
-          end
-
+        if event_revoke?(data)
+          user_badge = UserBadge.find_by(badge_id: badge.id, user_id: user.id)
+          BadgeGranter.revoke(user_badge)
         end
 
       elsif event_signup?(data)
-        user = User.find_by_email(data['member']['email'])
-        unless user.nil?
-          UserCustomField.create!(user_id: user.id, name: 'memberful_id', value: data['member']['id'])
-          user.save
-        end
+        UserCustomField.create!(user_id: user.id, name: 'memberful_id', value: data['member']['id'])
+        user.save
       end
 
       head :ok
     end
 
     private
+
+    def find_user(data)
+      if event_order?(data)
+        User.find_by_email(data['order']['member']['email'])
+      elsif event_signup?(data)
+        User.find_by_email(data['member']['email'])
+      end
+    end
 
     def event_order?(data)
       ['order.purchased', 'order.suspended'].include?(data['event'])
