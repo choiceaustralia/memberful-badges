c0b5b1697ef9f974c0f5a9f83fd0e53909c08e28
Rimian Perkins <hello@rimian.com.au>
2016-11-25 09:32:01 +1100

    use instace varable for data

--- app/controllers/memberful/web_hooks_controller.rb
+++ app/controllers/memberful/web_hooks_controller.rb
@@ -9,23 +9,23 @@ module Memberful
     end
 
     def create
-      data = JSON.parse(request.body.read)
-      user = find_user(data)
+      @data = JSON.parse(request.body.read)
+      user = find_user
 
       head :ok and return if user.nil?
 
-      if event_order?(data)
+      if event_order?
         badge = Badge.find_by_name('Consumer Defender')
 
-        BadgeGranter.grant(badge, user) if data['event'] == 'order.purchased'
+        BadgeGranter.grant(badge, user) if @data['event'] == 'order.purchased'
 
-        if event_revoke?(data)
+        if event_revoke?
           user_badge = UserBadge.find_by(badge_id: badge.id, user_id: user.id)
           BadgeGranter.revoke(user_badge)
         end
 
-      elsif event_signup?(data)
-        UserCustomField.create!(user_id: user.id, name: 'memberful_id', value: data['member']['id'])
+      elsif event_signup?
+        UserCustomField.create!(user_id: user.id, name: 'memberful_id', value: @data['member']['id'])
         user.save
       end
 
@@ -34,24 +34,24 @@ module Memberful
 
     private
 
-    def find_user(data)
-      if event_order?(data)
-        User.find_by_email(data['order']['member']['email'])
-      elsif event_signup?(data)
-        User.find_by_email(data['member']['email'])
+    def find_user
+      if event_order?
+        User.find_by_email(@data['order']['member']['email'])
+      elsif event_signup?
+        User.find_by_email(@data['member']['email'])
       end
     end
 
-    def event_order?(data)
-      ['order.purchased', 'order.suspended'].include?(data['event'])
+    def event_order?
+      ['order.purchased', 'order.suspended'].include?(@data['event'])
     end
 
-    def event_revoke?(data)
-      data['event'] == 'order.suspended'
+    def event_revoke?
+      @data['event'] == 'order.suspended'
     end
 
-    def event_signup?(data)
-      data['event'] == 'member_signup'
+    def event_signup?
+      @data['event'] == 'member_signup'
     end
   end
 end
